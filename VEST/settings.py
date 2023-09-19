b8f0b8a0b8fff401fd18f1ef7c1d31844dcdd11d
ritab <ritab@iseiea.org>
2023-09-19 18:25:26 -0400

    imported mimetypes

--- VEST/settings.py
+++ VEST/settings.py
@@ -18,6 +18,9 @@ from pathlib import Path
 from decouple import config
 from django.core.management.utils import get_random_secret_key
 
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
+
 env = environ.Env(DEBUG=(bool, False))
 environ.Env.read_env()
 
